f9d3a5469399ae7871332c4a5b8ecea1bda1bc15
Kyle-Hess <kyle.hess@my.jcu.edu.au>
2017-05-14 17:27:40 +1000

    added: - Themed settings, game over activities. - added some string resources on game activity

--- app/src/main/java/com/example/kyle/solarsystemeducationalapp/GameActivity.java
+++ app/src/main/java/com/example/kyle/solarsystemeducationalapp/GameActivity.java
@@ -62,6 +62,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_game);
 
         relativeGame = (RelativeLayout) findViewById(R.id.relativeGame);
